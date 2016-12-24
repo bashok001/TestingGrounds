2127aeea9eb87c3c6ab17dcf710d6521cc9a6a06
Ashok Bommisetti <ashok@shapesecurity.com>
2016-12-23 21:14:02 -0800

    Java 7 version.

--- src/com/test/grounds/PDFRefactor.java
+++ src/com/test/grounds/PDFRefactor.java
@@ -3,25 +3,27 @@ package com.test.grounds;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
-import java.util.stream.Stream;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 class PDFRefactor {
     public static void main(String args[]) {
         try {
             Path source = FileSystems.getDefault().getPath("tests/source");
-            Path destination = FileSystems.getDefault().getPath("tests/dest");
-            Stream<Path> files = Files.walk(source)
-                    .filter(path -> path.toFile().isFile())
-                    .filter(path -> path.toString().endsWith(".pdf"));
-            files.forEach(file -> {
-                try {
-                    Path parent = file.getParent();
-                    Path renamedFile = parent.resolve(parent.getFileName() + ".pdf");
-                    Files.move(file, renamedFile);
-                    Files.copy(renamedFile, destination.resolve(renamedFile.getFileName()));
-                } catch (IOException e) {
-                    e.printStackTrace();
+            final Path destination = FileSystems.getDefault().getPath("tests/dest");
+            Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file,
+                                                 BasicFileAttributes attrs) throws IOException {
+                    if (file.toString().endsWith(".pdf")) {
+                        Path parent = file.getParent();
+                        Path renamedFile = parent.resolve(parent.getFileName() + ".pdf");
+                        Files.move(file, renamedFile);
+                        Files.copy(renamedFile, destination.resolve(renamedFile.getFileName()));
+                    }
+                    return FileVisitResult.CONTINUE;
                 }
             });
         } catch (IOException ioEx) {
